404888c831f2885baf6013e729f7b171ae43c16f
Kay Strobach <info@kay-strobach.de>
2026-06-08 15:57:44 +0200

    [TASK] adjust migration
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

---
+++ lib/Migration/Version20260608155700.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace OCA\BesteSchule\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version20260608155700 extends SimpleMigrationStep {
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure
+     * @param array $options
+     * @return null|ISchemaWrapper
+     */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+
+        $tables = ['besteschule_grades', 'besteschule_finalgrades'];
+
+        foreach ($tables as $tableName) {
+            if ($schema->hasTable($tableName)) {
+                $table = $schema->getTable($tableName);
+                $column = $table->getColumn('value');
+                $column->setDefault('');
+            }
+        }
+
+        return $schema;
+    }
+}
